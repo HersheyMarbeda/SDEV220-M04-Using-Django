46d4a4f1ac94f01496ffdfa11b2a1e44c0c2ea97
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-22 11:46:07 -0500

    updates the front end

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 # list of all urls that are defined in the blog app that will be connected to html pages/views
 
 urlpatterns = [
-    # home page
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
